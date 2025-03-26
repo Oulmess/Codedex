5148baadb6f9736b7d24b73684c65c1a2a9c2b7e
HarounBoutounes <harounboutounes76@gmail.com>
2025-03-26 18:04:28 +0000

    Horoscope

---
+++ Horoscope.js
@@ -0,0 +1,30 @@
+let MoB = "April"
+let astroName
+
+if (MoB === "January") {
+  astroName = "Capricorn"
+} else if (MoB === "February") {
+  astroName = "Aquarius"
+} else if (MoB === "March") {
+  astroName = "Pisces"
+} else if (MoB === "April") {
+  astroName = "Aries"
+} else if (MoB === "May") {
+  astroName = "Taurus"
+} else if (MoB === "June") {
+  astroName = "Gemini"
+} else if (MoB === "July") {
+  astroName = "Cancer"
+} else if (MoB === "August") {
+  astroName = "Leo"
+} else if (MoB === "September") {
+  astroName = "Virgo"
+} else if (MoB === "October") {
+  astroName = "Libra"
+} else if (MoB === "November") {
+  astroName = "Scorpio"
+} else if (MoB === "December") {
+  astroName = "Sagittarius"
+} 
+
+console.log("You are a",astroName)
